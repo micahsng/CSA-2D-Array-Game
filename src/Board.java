9cad08073810f4cb3e8ac00415ac4c38d6544a7d
Dat1AZNBanana <Micahsng+github@gmail.com>
2018-03-15 13:17:05 -0700

    thing

--- src/Board.java
+++ src/Board.java
@@ -10,7 +10,6 @@ public class Board {
     private Set<String> gridClassNames;
     private String message;
     private JFrame frame;
-
     private static Random generator = new Random();
 
     private static final int DEFAULT_ROWS = 10;
