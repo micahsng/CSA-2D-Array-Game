36629bb40974fd78daa722f30d0b2cd772d4c0e0
Dat1AZNBanana <Micahsng+github@gmail.com>
2018-03-21 20:40:40 -0700

    remake

--- src/Connect3.java
+++ src/Connect3.java
@@ -1,11 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class Connect3 {
-    public int[] vertical = new int[3];
-    public int[] horizontal = new int[3];
-    public Connect3(){
+public class Connect3 extends JFrame{
+    public static GUI ui;
+    public int[] column = new int[3];
+    public int[] row = new int[3];
+
+    private Connect3(){
+        setTitle("Connect 3");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setResizable(false);
+        setLocationRelativeTo(null); // Centers in screen
+        setLayout(new FlowLayout());
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10, 10, 10, 10); // padding between edge of window
+        setLayout(layout);
 
     }
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) {new Connect3();}
 }
---
+++ src/Tictactoe.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Tictactoe{
+    public int[] column = new int[3];
+    public int[] row = new int[3];
+
+    private Tictactoe(){
+
+    }
+    public static void main(String[] args) {
+        /**boolean validInput = false;
+        while (validInput == false){
+            Scanner reader = new Scanner(System.in);
+            System.out.println("(1) Player or (2) Player?");
+            int playerChoice = reader.nextInt();
+            reader.close();
+            if(playerChoice == 1){
+                validInput = true;
+            }
+            else if(playerChoice == 2){
+                validInput = true;
+            }
+        }**/
+        System.out.println("  A   B   C");
+        System.out.println("1");
+    }
+}
