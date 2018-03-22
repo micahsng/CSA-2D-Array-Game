b1b48ee5948977a2e18184f217944fb26773fa9f
Dat1AZNBanana <Micahsng+github@gmail.com>
2018-03-22 09:30:24 -0700

    gamer

--- src/Tictactoe.java
+++ src/Tictactoe.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Tictactoe{
     public String[][] grid = new String[3][3];
     private int choice;
+    private boolean win;
     /**boolean validInput = false;
      while (validInput == false){
      System.out.println("(1) Player or (2) Player?");
@@ -15,9 +16,46 @@ public class Tictactoe{
      }
      }
      System.out.println("  A   B   C");
-     System.out.println(    "1");**/
+     System.out.println("1");**/
     private Tictactoe(){
-
+        for(int i = 0; i > 3; i++){
+            if(i == 1){
+                if(grid[i][i].equals(grid[i][i+1]) && grid[i][i].equals(grid[i][i+2])){
+                    win = true;
+                }
+                else if(grid[i][i].equals(grid[i+1][i+1]) && grid[i][i].equals(grid[i+2][i+2])){
+                    win = true;
+                }
+                else if(grid[i][i].equals(grid[i+1][i]) && grid[i][i].equals(grid[i+2][i])){
+                    win = true;
+                }
+            }
+            else if(i == 2){
+                if(grid[i][i].equals(grid[i][i-1]) && grid[i][i].equals(grid[i][i+1])){
+                    win = true;
+                }
+                else if(grid[i][i].equals(grid[i-1][i-1]) && grid[i][i].equals(grid[i+1][i+1])){
+                    win = true;
+                }
+                else if(grid[i][i].equals(grid[i-1][i]) && grid[i][i].equals(grid[i+1][i])){
+                    win = true;
+                }
+            }
+            else if(i == 3){
+                if(grid[i][i].equals(grid[i][i-2]) && grid[i][i].equals(grid[i][i-1])){
+                    win = true;
+                }
+                else if(grid[i][i].equals(grid[i-1][i-1]) && grid[i][i].equals(grid[i-2][i-2])){
+                    win = true;
+                }
+                else if(grid[i][i].equals(grid[i-2][i]) && grid[i][i].equals(grid[i-1][i])){
+                    win = true;
+                }
+            }
+            else{
+                win = false;
+            }
+        }
     }
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
@@ -25,3 +63,4 @@ public class Tictactoe{
         reader.close();
     }
 }
+    
